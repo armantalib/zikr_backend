421d39e08d8791dfa4a3a0bf6d5694bafeb2929f
apple <apple@apples-MacBook-Pro.local>
2024-12-23 23:49:43 +0500

    23 Dec

--- models/user.js
+++ models/user.js
@@ -55,7 +55,7 @@ const userSchema = new mongoose.Schema({
   type: {
     type: String,
     default: 'student',
-    enum: ['student', 'trainer','admin','hybrid']
+    enum: ['student', 'trainer','admin','hybrid','guest']
   },
   lang: {
     type: String,
